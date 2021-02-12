a32fbae12442d9037cc9ee93512776a2564c9313
NabiyevTR <NabiyeTR@gmail.com>
2021-02-12 09:58:14 +0300

    hometask 0301

---
+++ src/ru/geekbrains/ntr_0301/Apple.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.ntr_0301;
+
+public class Apple extends Fruit {
+    private static final float DEFAULT_WEIGHT = 1.0f;
+
+    public Apple() {
+        super(DEFAULT_WEIGHT);
+    }
+
+    public Apple(float weight) {
+        super(weight);
+    }
+}
---
+++ src/ru/geekbrains/ntr_0301/Fruit.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.ntr_0301;
+
+public abstract class Fruit {
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
